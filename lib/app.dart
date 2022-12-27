da6eacb9a56333c26d47cd592b0c8ac12581b008
Jimmyseesang <jimmyseesang@gmail.com>
2022-12-27 11:49:48 +0700

    update

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'gridview_card_page.dart';
+import 'gridview_page.dart';
+import 'home.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: GridViewPage(),
+    );
+  }
+}
